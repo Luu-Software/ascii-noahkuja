78ae44420d56467039ae1d3d63b2189d3a69d976
noahkuja <noahkuja17@gmail.com>
2026-06-17 15:45:33 -0300

    termine

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,29 @@ import { preguntar } from './lib/consola.ts';
 
 function repetirLetra(letra: string, cantidad: number): string {
   // COMPLETAR
+  let resultado : string = "";
+  
+let vecesrepetido : number = 0;
+
+while (vecesrepetido < cantidad){
+  resultado = resultado + letra
+  vecesrepetido = vecesrepetido + 1
+}
+return resultado;
 }
 
 let caracter: string = preguntar('¿Con que caracter vas a dibujar el cuadrado? ');
 let tamaño: number = Number(preguntar('¿De qué tamaño? '));
 
+let filas : number = 0;
+
+
+
+while (filas < tamaño ){
+let linea : string = repetirLetra(caracter, tamaño);
+
+console.log (linea);
+
+filas = filas + 1
+}
 // COMPLETAR
